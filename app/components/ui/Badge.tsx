9767bf50278d6dac0a415d62e576efae480a9a3b
fojacs653 <simmsz@gmail.com>
2025-09-20 13:38:02 -0400

    Delete app directory

--- app/components/ui/Badge.tsx
+++
@@ -1,6 +0,0 @@
-
-import { HTMLAttributes } from 'react'
-import clsx from 'clsx'
-export default function Badge({ className, ...props }: HTMLAttributes<HTMLSpanElement>){
-  return <span className={clsx('inline-block rounded-full border border-[#2b2b3a] bg-[#1e1e2a] px-3 py-1 text-xs text-[#9aa0aa]', className)} {...props} />
-}
